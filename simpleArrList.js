f1450dcd77a3f4bfd6c9d9373f06cebf9f044131
Kosta Malsev <51965607+KostaMalsev@users.noreply.github.com>
2022-05-03 13:29:09 +0300

    Update simpleArrList.js

--- simpleArrList.js
+++ simpleArrList.js
@@ -1,2 +1,52 @@
-
-
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var addTwoNumbers = function(l1, l2) {
+    
+    console.log(l1,l2);
+    
+    function getNumberFromLinkedList(l)
+    {
+        
+        let number = 0;
+        console.log('linked list',l.next.val)
+        for(var i=l.length; i>0; i--)
+            {
+                console.log(number)
+                number = l[i]*Math.pow(10,l.length - i);
+            }
+        return number;
+    }
+    
+    function buildLinkedList(number)
+    {
+        let nm = number;
+        let lnm = [];
+        let i=0;
+        while(nm > 1)
+            {
+                lnm.push(nm % 10);
+                nm = nm/10;
+            }
+        return lnm;
+    }
+    
+    console.log(l1,l2);
+    
+    let num1 = getNumberFromLinkedList(l1);
+    let num2 = getNumberFromLinkedList(l2);
+    
+    let sum = num1 + num2;
+    console.log(sum)
+    console.log(buildLinkedList(sum));
+    //return buildLinkedList(sum);
+};
