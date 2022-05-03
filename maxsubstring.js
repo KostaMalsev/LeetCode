945086319728b790ce7ce8673e6ec84bc104c2fd
Kosta Malsev <51965607+KostaMalsev@users.noreply.github.com>
2022-05-03 16:33:12 +0300

    Update maxsubstring.js

--- maxsubstring.js
+++ maxsubstring.js
@@ -1,55 +1,36 @@
-/**
- * @param {string} s
- * @return {number}
- */
+/** * @param {string} s * @return {number} */
 var lengthOfLongestSubstring = function(s) {
-    
-    let hasBeenArr = [];
-    hasBeenArr.fill(-1,0,127);
-    let sstring = '';
-    
-    
-    while(s.length>0)
-        {
-            let sLength = s.length;
-            let i=0;
-            hasBeenArr.fill(-1,0,127);
-
-                while(i < sLength)
-                {
-                    let hasBeen = hasBeenArr[s[i].charCodeAt(0)];
-                    if( hasBeen > 0 ){
-                        if(sstring.length < i){
-                            //Store the longest string:
-                            sstring = s.substring(0, i-1);
-                        }
-                            
-                        //Slice the relevent part for future search iteration:
-                        s = s.substring(hasBeen,s.length);
-                        
-                        break; //exit the inner loop
-                    }else{
-                        //Store in hash
-                        hasBeenArr[s[i].charCodeAt(0)] = i;
-                    }
-                i++;
-                }
-            
-            //Residue string:
-            if( i==sLength && s.length>0){
-                console.log(s,i,hasBeenArr[s[s.length-1].charCodeAt(0)],sLength);
-                let hasBeen = hasBeenArr[s[s.length-1].charCodeAt(0)];
-                
-                if(sstring.length == 0 )
-                    {
-                        
-                        if(hasBeen <= sLength-1 && hasBeen > 0)
-                            sstring = s.substring(hasBeen,s.length-1);
-                        else sstring = s;
-                    }
-                break;            
-            }
+  let hasBeenArr = new Array(128).fill(-1);
+  let sstring = '';
+  while (s.length > 0) {
+    let sLength = s.length;
+    let i = 0;
+    hasBeenArr.fill(-1, 0, 127);
+    while (i < sLength) {
+      let hasBeen = hasBeenArr[s[i].charCodeAt(0)];
+      console.log('itr', i, s, hasBeen, s, sLength);
+      if (hasBeen > -1) {
+        if (sstring.length < i) {
+          //Store the longest string:                            
+          sstring = s.substring(0, i - 1);
+        } //Slice the relevent part for future search iteration:                        
+        s = s.substring(hasBeen + 1, s.length - 1);
+        break; //exit the inner loop                                            
+      } else {
+        //Store in hash                        
+        hasBeenArr[s[i].charCodeAt(0)] = i;
+        //In case we at the end of the string:                       
+        if (i == sLength - 1 && (sstring.length < s.length)) {
+          console.log(s)
+          sstring = s;
+          break;
         }
-    console.log(sstring)
-    return sstring.length;
+      }
+      i++;
+    }
+    if (i == sLength) { //console.log(s,'max strig:',sstring)              
+      break;
+    }
+  }
+  console.log(sstring) return sstring.length;
 };
