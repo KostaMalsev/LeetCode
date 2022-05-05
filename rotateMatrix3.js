f9fb0fac1cf05f66f6e8d3397a10238e400a0b1c
Kosta Malsev <51965607+KostaMalsev@users.noreply.github.com>
2022-05-05 21:24:08 +0300

    Update rotateMatrix3.js

--- rotateMatrix3.js
+++ rotateMatrix3.js
@@ -13,6 +13,8 @@ var rotate = function(matrix) {
         return [Math.max(row - D, offset), col - (row - offset - D)];
     }
   }
+  
+  
   let N = matrix[0].length;
   console.log(matrix);
   let tmp = -1,
@@ -23,12 +25,16 @@ var rotate = function(matrix) {
     nextCol = 0;
   let dim = N;
   let ddim = N;
+  
   while (dim > 0) {
+    
     row = N - ddim;
     D = dim - 1;
+    
     for (let ii = 0; ii < dim - 1; ii++) {
       col = N - ddim + ii;
       tmp = -1;
+      
       for (let ind = 0; ind < 4; ind++) {
         /*Generate next positon*/
         [nextRow, nextCol] = next(row, col, dim - 1, ind, D, N - ddim);
@@ -38,13 +44,18 @@ var rotate = function(matrix) {
         } else {
           valToMove = tmp;
           tmp = matrix[nextRow][nextCol];
-        } /*Move the value to next position*/ /*console.log('curr:',row,col,'next:',nextRow,nextCol,'||',ind,ddim-1,D,N - ddim,dim,matrix[nextRow][nextCol]);*/
+        } 
+        /*Move the value to next position*/ 
+        /*console.log('curr:',row,col,'next:',nextRow,nextCol,'||',ind,ddim-1,D,N - ddim,dim,matrix[nextRow][nextCol]);*/
         matrix[nextRow][nextCol] = valToMove;
         row = nextRow;
         col = nextCol;
       }
-    } /*If got to the center, stop:*/
+    } 
+    
+    /*If got to the center, stop:*/
     let needToStop = N % 2 == 0 ? dim == 2 : dim == 1;
+    
     if (needToStop) {
       /*console.log('got to the middle',row, col, dim, N)*/
       return matrix;
