cfbc1f1de3c9ac389e5eaefdc659722eb97e2a5e
Kosta Malsev <51965607+KostaMalsev@users.noreply.github.com>
2022-05-10 14:34:59 +0300

    Update histogram.js

--- histogram.js
+++ histogram.js
@@ -1,1 +1,36 @@
-
+/**
+ * @param {number[]} heights
+ * @return {number}
+ */
+var largestRectangleArea = function(heights) {
+    
+    let N = heights.length;
+    
+    function miN(strt,stop)
+    {
+        
+    }
+    
+    function area(strt,stop)
+    {
+        let min_=-1;
+        for(let i=strt;i<stop;i++)
+            {
+                if(min_==-1)
+                    {
+                        min_ = heights[i];
+                    }else{
+                        if(min_ < heights[i]) min_ = heights[i];
+                    }
+            }
+        
+    }
+    
+    for(let wsize=1; wsize<=N; wsize++)
+        {
+            for(let strtInd=0; strtInd < N; strtInd++)
+                {
+                    
+                }
+        }
+};
