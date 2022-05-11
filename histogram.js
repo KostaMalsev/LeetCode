4e48cf393a46177d5413a44b48be61f0c32519dc
Kosta Malsev <51965607+KostaMalsev@users.noreply.github.com>
2022-05-11 11:15:16 +0300

    Update histogram.js

--- histogram.js
+++ histogram.js
@@ -6,34 +6,74 @@ var largestRectangleArea = function(heights) {
     
     let N = heights.length;
     
+    
     function miN(strt,stop)
     {
         let min_= heights[strt];
-        let nz = 0;
         
         for(let i=strt;i<=stop;i++)
             {
                 if( (min_ > heights[i]) && (heights[i]>0) ) {
                     min_ = heights[i];
                 }
-                //Count zeros
-                if(heights[i]==0) nz++;
                 
             }
         
-        return [nz, min_];
+        return min_;
     }
+    
     
     
     function area(strt,stop)
     {
-        [nz, height] = miN(strt,stop);
-        let width = stop - strt+1-nz;
-        let area = height*(width);//*Math.max(1,width);
         
-        console.log(strt,stop,'||',height, width, area,nz);
+        let area = 0;
+        let mxAreal = 0;
+        let zfound = false;
+        let minHeight=heights[strt];
+        let lstZ = strt;
         
-        return area;
+        let height, width;
+        
+        //Start collecting areas:
+        for(let i=strt;i<=stop;i++)
+            {                
+                //If zero is reached, restart collecting and update the max
+                if(heights[i] == 0)
+                {
+                       //Get minimal height of the group
+                       height =  miN(strt,i);
+                       width = Math.max(i - lstZ - 1, 1);
+                       area = height * width;
+                    
+                        if(mxAreal < area) mxAreal = area;
+                       
+                       area = 0;
+                       lstZ = i;
+                       zfound = true;
+                }
+                
+                
+                //Store the minimal height:
+                if( heights[i] < minHeight && heights[i] > 0 ) minHeight = heights[i];
+                
+                if(mxAreal < area) mxAreal = area;   
+            }
+    
+        
+        
+        //If no zeros found, use the minimal
+        if(!zfound)
+            {
+                height = minHeight;
+                width = stop - strt + 1;
+                area = height * width;
+                mxAreal = area;
+            }
+        
+        //console.log(strt,stop,'||',mxAreal,height,width);
+        
+        return mxAreal;
         
     }
     
