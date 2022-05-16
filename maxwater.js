b71af4155ae455ede8e004dc3565401046b7c575
Kosta Malsev <51965607+KostaMalsev@users.noreply.github.com>
2022-05-16 22:15:00 +0300

    Update maxwater.js

--- maxwater.js
+++ maxwater.js
@@ -1,2 +1,13 @@
-
-
+//Run on all widths
+    for(let offset=0; offset<N-1;offset++)
+    {
+        for(let i=N-offset-1;i>0;i--)
+        {
+              let wd = i;
+              //Calculate area between the borders:  
+              let area = Math.min(height[offset],height[i+offset]) * wd;  
+              if(maxArea< area) maxArea = area;
+            //console.log(offset,offset + i,'||Area=',area,'maxArea=',maxArea);
+         }
+    }
+    return maxArea;
