ba20ade31100681ba38208f2f609f000a08fb2a9
Kosta Malsev <51965607+KostaMalsev@users.noreply.github.com>
2022-06-28 17:24:24 +0300

    Update directGraph.js

--- directGraph.js
+++ directGraph.js
@@ -1,2 +1,36 @@
-
-
+/**
+ * @param {number} n
+ * @param {number[][]} edges
+ * @param {number} source
+ * @param {number} destination
+ * @return {boolean}
+ */
+var leadsToDestination = function(n, edges, source, destination) {
+    
+    
+    let hs={};
+    let count = 0;
+    let valid = true;
+    
+    function move(node,nexta,count,path)
+    {
+        
+        if(node == destination)
+            {
+                count++;
+                console.log('reached destination');
+                return;
+            }else{
+                
+                if(hs[nexta.toStirng()]!==undefined)
+                    {
+                        valid = false;
+                        return;
+                    }else{
+                        
+                        
+                    }
+            }
+    }
+    
+};
