ce404215795dc2dd00ff79aaa0682b9dacbddda0
Kosta Malsev <51965607+KostaMalsev@users.noreply.github.com>
2022-07-24 19:37:17 +0300

    Update objectOriented.js

--- objectOriented.js
+++ objectOriented.js
@@ -1,2 +1,33 @@
+// MenuInterface.js
+class MenuInterface {
+  constructor() {
+    if(!this.getItems) {
+      throw new Error("Menu's must have items!");
+    }
+  }
+}
 
-
+// ---
+
+// file index.js
+import MenuInterface from './MenuInterface';
+
+class Menu extends MenuInterface {
+  // no items
+}
+export default new Menu() // Error "Menu's must have items!"
+
+
+// ---
+
+// file  baseMenu.js
+class BaseMenu extends MenuInterface {
+  constructor() {
+    super();
+    this.items = [];
+  }
+  get getItems() {
+    return items;
+  }
+}
+export default new BaseMenu();  // No Error, has the method
