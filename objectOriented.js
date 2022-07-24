68cf465c3b401bdceb8d8f81eec94dfbc1ebbd10
Kosta Malsev <51965607+KostaMalsev@users.noreply.github.com>
2022-07-24 19:42:04 +0300

    Update objectOriented.js

--- objectOriented.js
+++ objectOriented.js
@@ -18,6 +18,24 @@ class Menu extends MenuInterface {
 export default new Menu() // Error "Menu's must have items!"
 
 
+//---------------------
+//requiring path and fs modules
+const path = require('path');
+const fs = require('fs');
+//joining path of directory 
+const directoryPath = path.join(__dirname, 'Documents');
+//passsing directoryPath and callback function
+fs.readdir(directoryPath, function (err, files) {
+    //handling error
+    if (err) {
+        return console.log('Unable to scan directory: ' + err);
+    } 
+    //listing all files using forEach
+    files.forEach(function (file) {
+        // Do whatever you want to do with the file
+        console.log(file); 
+    });
+});
 // ---
 
 // file  baseMenu.js
